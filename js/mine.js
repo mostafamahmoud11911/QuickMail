677b364a13ae36f329bdabce579a5d27e0c803eb
mostafamahmoud11911 <mm0766633@gmail.com>
2025-11-15 18:59:23 +0400

    edit

--- js/mine.js
+++ js/mine.js
@@ -12,30 +12,41 @@ const outputText = document.getElementById("outputText");
 
 let finalTemplate = "";
 
+function checkInputsFilled() {
+  return (
+    greeting.value !== "Select a greeting..." &&
+    title.value !== "Select a title..." &&
+    username.value.trim() !== "" &&
+    opener.value !== "Select an opener..." &&
+    body.value.trim() !== "" &&
+    closure.value !== "Select a closing..." &&
+    sender.value.trim() !== ""
+  );
+}
+
 function generateTemplate() {
   const greetingLine = `${greeting.value} ${title.value} ${username.value},`;
-
-  const openerLine = opener.value ? `\n${opener.value}.` : "";
-
-  const bodyLine = body.value ? `\n\n${body.value}.` : "";
-
-  const closureLine = closure.value ? `\n\n${closure.value}\n` : "";
-
+  const openerLine = opener.value ? `\n${opener.value}.\n` : "";
+  const bodyLine = body.value ? `\n${body.value}.\n` : "";
+  const closureLine = closure.value ? `\n${closure.value}\n` : "";
   const senderLine = sender.value ? `${sender.value}` : "";
 
+
+
   finalTemplate = `${greetingLine}${openerLine}${bodyLine}${closureLine}${senderLine}`;
+
+  if (checkInputsFilled()) {
+    outputContainer.style.display = "block";
+    outputText.value = finalTemplate;
+  } else {
+    outputContainer.style.display = "none";
+  }
 }
 
 generate.addEventListener("click", generateTemplate);
 
-
+generate.addEventListener("click", generateTemplate);
 
 copy.addEventListener("click", () => {
-  if (finalTemplate.length === 0) {
-    outputContainer.style.display = "none";
-  } else {
-    navigator.clipboard.writeText(finalTemplate);
-    outputContainer.style.display = "block";
-    outputText.value = finalTemplate;
-  }
+  navigator.clipboard.writeText(finalTemplate);
 });
